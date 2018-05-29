8ba0ab274f1cb7777eb9cce8242000d1576f0620
ajp8164 <andyphillipson@gmail.com>
2018-05-29 12:21:42 -0400

    Fix missing reference, bump version 0.0.4

--- plugin/services/helloService.js
+++ plugin/services/helloService.js
@@ -3,7 +3,8 @@
 angular.module('owsWalletPlugin.services').service('helloService', function (Session) {
 
   var root = {};
-
+  var session;
+  
   root.init = function(cb) {
     session = Session.getInstance();
     root.appletName = session.plugin.header.name;
