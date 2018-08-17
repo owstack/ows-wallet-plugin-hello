f15435b38f7a2b71632583f925ef7089aa0e6680
ajp8164 <andyphillipson@gmail.com>
2018-08-17 11:09:33 -0400

    Allow use of app css

--- util/build.js
+++ util/build.js
@@ -84,7 +84,13 @@ function createIndexHtml() {
   config.plugincss = '';
   if (plugin.header.kind == 'applet') {
     config.plugincss += '\n  <link rel="stylesheet" type="text/css" href="' + config.rootRelative + 'css/animate.min.css">'; // Animate.css
-    config.plugincss += '\n  <link rel="stylesheet" type="text/css" href="' + config.rootRelative + 'css/ows-wallet-applet.css">'; // Applet lib css
+
+    if (plugin.linkHostCss == true) {
+      config.plugincss += '\n  <link rel="stylesheet" type="text/css" href="' + config.rootRelative + 'css/ows-wallet.css">'; // Host app css
+    } else {
+      config.plugincss += '\n  <link rel="stylesheet" type="text/css" href="' + config.rootRelative + 'css/ows-wallet-applet.css">'; // Applet lib css
+    }
+
     config.plugincss += '\n  <link rel="stylesheet" type="text/css" href="css/main.css">'; // Plugin defined css
   }
 
